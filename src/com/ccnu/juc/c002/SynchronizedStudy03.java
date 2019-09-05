7f186be7afe5c6aaf24fc72967d56bf80052f85e
XiongqiChang <2838159948@qq.com>
2019-09-05 09:34:13 +0800

    可å重入锁

--- src/com/ccnu/juc/c002/SynchronizedStudy03.java
+++ src/com/ccnu/juc/c002/SynchronizedStudy03.java
@@ -2,6 +2,7 @@ package com.ccnu.juc.c002;
 
 /**
  * 对于Synchronized的话这个锁是可重入锁的
+ * 可重入锁指的是对于当前对象一把锁可以锁定另外一把锁
  */
 public class SynchronizedStudy03 {
 
@@ -9,24 +10,27 @@ public class SynchronizedStudy03 {
         System.out.println(" m1 start");
         try {
             Thread.sleep(2000);
+            m2();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        System.out.println("m1 end");
     }
 
-    synchronized  int m2(){
+    synchronized  void  m2(){
         System.out.println("m2 start");
-        int i = 1;
         try {
-//            int j = 1 / 0;
-            return i;
             Thread.sleep(2000);
         } catch (InterruptedException e) {
-            i = 2;
+
             e.printStackTrace();
-        }finally {
-            i = 3;
-            return i;
         }
+        System.out.println("m2  end");
     }
+
+    public static void main(String[] args) {
+        SynchronizedStudy03 synchronizedStudy03 = new SynchronizedStudy03();
+        synchronizedStudy03.m1();
+    }
+
 }
